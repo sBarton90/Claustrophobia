b3311c02196ac75a498aa34b2dfdad048c4b107d
Ryan Jagoe <jagoeoverlord@gmail.com>
2021-11-10 16:11:42 -0500

    Fixed double jumping
    25% more movement speed since the player felt slow

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,25 +7,23 @@ public class PlayerMovement : MonoBehaviour
     public Transform Player;
 
     float JumpPower = 4;
-    float MoveSpeed = 2;
+    float MoveSpeed = 2.5f;
 
     float Grounded; //Will be 1 or 1.75, Grounded / not Grounded (Nerfs air movement)
     Vector3 Speed;
-    
-    void Start()
-    {
-
-    }
 
     void Jump()
     {
-        if (Grounded == 1)
+        RaycastHit2D Down = Physics2D.Raycast(Player.position - new Vector3(0, 1.2f), -Player.up, 0.1f);
+        if (Down)
         {
+            Debug.Log("Hit " + Down.collider.gameObject.name);
             Rigidbody2D Body = Player.GetComponent<Rigidbody2D>();
             //Work-around since you can't specifically set x and y of velocity directly... 
             Vector3 Temp = Speed;
             Temp.y = JumpPower;
             Body.velocity = Temp; //Body.velocity sets a constant velocity so the player doesn't speed up out of control
+            new WaitForSeconds(0.1f); // Wait for the player to be in the air before saying the player isn't grounded
         }
     }
 
@@ -33,7 +31,7 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
-     
+
         // Movement controls //
         // W - Up (Replaced W with Spacebar)
         // S - Down (Not in use unless we want to make player fall faster when "s" is pressed)
@@ -46,57 +44,62 @@ public class PlayerMovement : MonoBehaviour
         //Get horizontal and vertical speed for fixing fall speed and preventing gliding while falling
         Vector3 Speed = Body.velocity;
 
-        //Raycast to detect if the player is on the ground
-        RaycastHit2D Down = Physics2D.Raycast(Player.transform.position - new Vector3(0, 0.7f), Player.up * -1, 0.05f);
-        if (Down) { 
-            Grounded = 1;
-        } else {
-            Grounded = 1.75f;
-        }
-
         //GetKeyDown runs code once when pressed. GetKey runs the code repeatedly while key is pressed. (GetKey could be used for autojumping)
         if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+
+        //Move Left
+        if (Input.GetKey("a"))
+        {
+            RaycastHit2D Left = Physics2D.Raycast(Player.transform.position - new Vector3(0.55f, 0.66f), -Player.right, 0.1f);
+            //Debug.DrawRay(Player.transform.position - new Vector3(0.55f, 0.66f), -Player.right * 0.1f, Color.red);
+
+            Vector3 Temp = Speed;
+            Temp.x = -MoveSpeed;
+            Body.velocity = Temp;
+
+            if (Input.GetKeyDown(KeyCode.Space))
             {
                 Jump();
             }
 
-            //Move Left
-            if (Input.GetKey("a"))
+            if (Left)
             {
-                //Disable moving left if the player can't go further left. This stops the player from pushing against the wall and floating on it.
-                RaycastHit2D Left = Physics2D.Raycast(Player.transform.position - new Vector3(0.5f, 0.66f), Player.right * -1, 0.00001f);
-
-                if (!Left)
-                {
-                    Vector3 Temp = Speed;
-                    Temp.x = -MoveSpeed / Grounded;
-                    Body.velocity = Temp;
-
-                    if (Input.GetKeyDown(KeyCode.Space)) 
-                    {
-                        Jump();
-                    }
-                }
+                Vector3 Temp2 = Speed;
+                Temp.x = 0;
+                Body.velocity = Temp;
             }
 
-            //Move Right
-            if (Input.GetKey("d"))
-            {
-                RaycastHit2D Right = Physics2D.Raycast(Player.transform.position + new Vector3(0.5f, -0.66f), Player.right, 0.00001f);
-                if (!Right)
-                {
-                    Vector3 Temp = Speed;
-                    Temp.x = MoveSpeed / Grounded;
-                    Body.velocity = Temp;
+        }
 
-                    if (Input.GetKeyDown(KeyCode.Space))
-                    {
-                        Jump();
-                    }
-                }
+        //Move Right
+        if (Input.GetKey("d"))
+        {
+            RaycastHit2D Right = Physics2D.Raycast(Player.transform.position + new Vector3(0.5f, -0.66f), Player.right, 0.1f);
+            //Debug.DrawRay(Player.transform.position + new Vector3(0.5f, -0.66f), Player.right * 10, Color.red);
+
+            Vector3 Temp = Speed;
+            Temp.x = MoveSpeed;
+            Body.velocity = Temp;
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Jump();
             }
+
+            if (Right) // Cancel out velocity
+            {
+                Vector3 Temp2 = Speed;
+                Temp.x = 0;
+                Body.velocity = Temp;
+            }
+
         }
 
     }
+
+}
 
 
