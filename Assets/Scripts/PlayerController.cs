169928ae8d0fbba9f585943f8cd2aaad5c1eb79d
Shane <shanecb@hotmail.ca>
2021-11-11 12:15:13 -0500

    Added ladder climbing, stopped walljumps and wall sticking

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,22 +7,28 @@ public class PlayerController : MonoBehaviour
 {
     Vector2 moveInput;
     Rigidbody2D myRigidbody;
-    CapsuleCollider2D myCapsuleCollider;
+    CapsuleCollider2D myBodyCollider;
+    BoxCollider2D myFeetCollider;
+    float gravityScaleAtStart;
 
     [Header("Movement Variables")]
     [SerializeField] float runSpeed = 5;
     [SerializeField] float jumpSpeed = 5;
+    [SerializeField] float climbSpeed = 4;
     
     void Start() // Create a reference to our own rigidbody/collider on start.
     {
         myRigidbody = GetComponent<Rigidbody2D>();
-        myCapsuleCollider = GetComponent<CapsuleCollider2D>();
+        myBodyCollider = GetComponent<CapsuleCollider2D>();
+        myFeetCollider = GetComponent<BoxCollider2D>();
+        gravityScaleAtStart = myRigidbody.gravityScale;
     }
 
     
     void FixedUpdate() // Run our two methods every frame
     {
         Run();
+        ClimbLadder();
         FlipSprite();
     }
 
@@ -31,9 +37,9 @@ public class PlayerController : MonoBehaviour
     }
 
     void OnJump(InputValue value) { // When pressing the jump key of the input system.
-        
-        if (!myCapsuleCollider.IsTouchingLayers(LayerMask.GetMask("Ground"))) { // If not touching the ground, exit method to prevent double jumping.
-            return;
+
+        if (!myFeetCollider.IsTouchingLayers(LayerMask.GetMask("Ground"))) {
+            return; // If not touching the ground, exit method to prevent double jumping.
         }
 
         if (value.isPressed) { // If we press the space key to jump {
@@ -44,6 +50,16 @@ public class PlayerController : MonoBehaviour
     void Run() { // Apply the value of controller input to rigidboy's velocity multiplied by our runSpeed
         Vector2 playerVelocity = new Vector2(moveInput.x * runSpeed, myRigidbody.velocity.y); // Set velocity of players x without changing the y value.
         myRigidbody.velocity = playerVelocity;
+    }
+
+    void ClimbLadder() { // if Player is able to climb ladder, do so.
+        if (!myFeetCollider.IsTouchingLayers(LayerMask.GetMask("Ladder"))) { // If not touching the ladder layer, unable to climb.
+            myRigidbody.gravityScale = gravityScaleAtStart; // if not climbing a ladder, player gravity is normal
+            return;
+        }
+        Vector2 climbVelocity = new Vector2(myRigidbody.velocity.x, moveInput.y * climbSpeed);
+        myRigidbody.velocity = climbVelocity;
+        myRigidbody.gravityScale = 0f; // if climbing a ladder, gravitiy is 0. To prevent sliding down the ladder.
     }
 
     void FlipSprite() { // Flip our sprite whenever we are moving in that direction.
