0b864a647f6c9840d7ea61b8639cb79a141fb24c
Ryan Jagoe <jagoeoverlord@gmail.com>
2021-12-08 13:26:47 -0500

    Change death screen & Fade to main menu

--- Assets/Scripts/Oxygen.cs
+++ Assets/Scripts/Oxygen.cs
@@ -1,21 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement; //For loading the scene after death
 using UnityEngine.UI; // This is for getting image componenet of the bar
 
 public class Oxygen : MonoBehaviour
 {
     [SerializeField] AudioClip pickupChime;
+    [SerializeField] AudioSource Footsteps;
+    [SerializeField] AudioSource BGAudio;
 
-    [SerializeField] 
+    [SerializeField]
     private CanvasGroup DeathCan;
-    [SerializeField] 
+    [SerializeField]
+    private CanvasGroup FakeMain;
+    [SerializeField]
     GameObject Bar;
     public PlayerController MoveScript;
 
     float MaxO2 = 120;
     public float O2 = 120; //120 is the seconds of oxygen left (2 minutes)
-    
+
 
     private void Start()
     {
@@ -24,10 +29,10 @@ public class Oxygen : MonoBehaviour
 
     void Update()
     {
-        if(O2 > 0){
+        if (O2 > 0)
+        {
             MoveScript.enabled = true; // Allow movement        
             DeathCan.alpha = 0f; //Hide death canvas while alive
-            DeathCan.interactable = false; //Disable buttons on canvas while not visible
 
             float Size = O2 / MaxO2;
             O2 -= Time.deltaTime;
@@ -36,9 +41,26 @@ public class Oxygen : MonoBehaviour
         }
         else
         {
+            //Mute audio (footsteps audio persisted while movement was disabled if player was moving as they died)
+            Footsteps.mute = true;
             MoveScript.enabled = false;
-            DeathCan.alpha = 1f;
-            DeathCan.interactable = true;
+            if (DeathCan.alpha < 1)
+            {
+                DeathCan.alpha += Time.deltaTime / 3;
+            }
+            else
+            {
+                if(FakeMain.alpha < 1)
+                {
+                    BGAudio.mute = true;
+                    FakeMain.alpha += Time.deltaTime; //Fake menu shows up faster to mask the fact that it's fake 
+                }
+                else //Load Main
+                {
+                    SceneManager.LoadScene("MainMenu");
+                }
+            }
+
         }
     }
 
@@ -47,7 +69,7 @@ public class Oxygen : MonoBehaviour
         if (collision.gameObject.name.Contains("Oxygen"))
         {
             O2 += 15; //Add 15 seconds of oxygen
-            if(O2 > 120) { O2 = 120; } // Prevent o2 from going over max
+            if (O2 > 120) { O2 = 120; } // Prevent o2 from going over max
             GetComponent<AudioSource>().PlayOneShot(pickupChime);
             Destroy(collision.gameObject);
         }
